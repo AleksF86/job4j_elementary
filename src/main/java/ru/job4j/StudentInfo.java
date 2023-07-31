8382a8235acbf263242af2450172f0a1ac85693d
AleksF86 <aafedorov7@gmail.com>
2023-07-31 21:58:30 +0300

    4.1. Разбор первой программы. [#159033 # [#159033]]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Aleksey");
-        System.out.println("19.08.1985");
+        System.out.println("19.08.1986");
     }
 }
